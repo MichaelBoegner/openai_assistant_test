a711cf024038aca29f510309172244adddba8821
michaelboegner <cmikeb@gmail.com>
2024-05-17 19:02:48 -0400

    First successful iteration of Assistants code_interpreter using gpt-3.5-turbo-16k. Not clear yet on why my API token doesnt appear to list/have access to the latest model versions.

--- main.py
+++ main.py
@@ -5,41 +5,37 @@ from openai import AssistantEventHandler
 load_dotenv()
 client = OpenAI()
 
-completion = client.chat.completions.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
-    {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
-  ]
+from openai import OpenAI
+client = OpenAI()
+  
+
+assistant = client.beta.assistants.create(
+  name="Math Tutor",
+  instructions="You are a personal math tutor. Write and run code to answer math questions.",
+  tools=[{"type": "code_interpreter"}],
+  model="gpt-3.5-turbo-16k",
 )
 
-print(completion.choices[0].message)
+thread = client.beta.threads.create()
 
-# assistant = client.beta.assistants.create(
-# name="Math Tutor",
-# instructions="You are a personal math tutor. Write and run code to answer math questions.",
-# tools=[{"type": "code_interpreter"}],
-# model="gpt-4o",
-# )
+message = client.beta.threads.messages.create(
+  thread_id=thread.id,
+  role="user",
+  content="I need to solve the equation `3x + 11 = 14`. Can you help me?"
+)
 
-# thread = client.beta.threads.create()
+run = client.beta.threads.runs.create_and_poll(
+  thread_id=thread.id,
+  assistant_id=assistant.id,
+  instructions="Please address the user as Jane Doe. The user has a premium account."
+)
 
-# message = client.beta.threads.messages.create(
-# thread_id=thread.id,
-# role="user",
-# content="I need to solve the equation `3x + 11 = 14`. Can you help me?"
-# )
+if run.status == 'completed': 
+  messages = client.beta.threads.messages.list(
+    thread_id=thread.id
+  )
+  print(messages)
+else:
+  print(run.status)
 
-# run = client.beta.threads.runs.create_and_poll(
-#   thread_id=thread.id,
-#   assistant_id=assistant.id,
-#   instructions="Please address the user as Jane Doe. The user has a premium account."
-# )
 
-# if run.status == 'completed': 
-#   messages = client.beta.threads.messages.list(
-#     thread_id=thread.id
-#   )
-#   print(messages)
-# else:
-#   print(run.status)
